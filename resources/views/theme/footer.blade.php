d43091c3d4b0472a1c561128bd731eba56918dfc
taufiq-cancode <taofeekadekunle2@gmail.com>
2024-04-18 06:45:15 +0100

    Fix footer

--- resources/views/theme/footer.blade.php
+++ resources/views/theme/footer.blade.php
@@ -1,12 +1,12 @@
 <footer id="footer" class="mt-0 bg-color-grey border-0">
     <div class="container mb-4 pt-4">
         <div class="row py-5">
-            <div class="col-6 col-lg-6 mb-5 mb-lg-0">
+            <div class="col-12 col-lg-6 mb-5 mb-lg-0">
                 <img src="img/iguru-long-black.png" width="200px" class="img-fluid mb-2" alt="FamCraft">
                 <p>At iGuruWeb,we are a technological based company which specializes in the creation,development & re-selling of localized gaming contents,software development,iGaming consultancy services,business partnership. </p>
             </div>
 
-            <div class="col-md-6 col-lg-3 mb-5 mb-lg-0">
+            <div class="col-md-12 col-lg-3 mb-5 mb-lg-0">
                 <h5 class="text-5 text-transform-none font-weight-semibold text-color-dark mb-4">Contact Details</h5>
                 <p class="text-4 mb-0">123 Porto Blvd, Suite 100</p>
                 <p class="text-4 mb-0">New York, NY</p>
@@ -14,7 +14,7 @@
                 <p class="text-4 mb-0">Email: <a href="mailto:mail@example.com" class="text-color-primary">mail@example.com</a></p>
             </div>
             
-            <div class="col-6 col-lg-3 mb-5 mb-lg-0">
+            <div class="col-12 col-lg-3 mb-5 mb-lg-0">
                 <h5 class="text-5 text-transform-none font-weight-semibold text-color-dark mb-4">Links</h5>
                 <ul class="list list-icons list-icons-sm">
                     <li><i class="fas fa-angle-right"></i><a href="page-faq.html" class="text-color-hover-primary ms-1"> About Us</a></li>
